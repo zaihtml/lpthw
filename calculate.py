1213a041da8cb8534a15348f6a3e8d2c1aa04969
zaihtml <67802675+zaihtml@users.noreply.github.com>
2021-02-11 19:10:53 +0000

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,9 @@
+# Days off
+
+print "I will now calculate how many days I have off."
+print 5 + 2 * 2
+
+print "Now I will see if this is less than or greater than or the same as the days I was sick with coronavirus."
+
+print 5 + 2 * 2 >= 10
+print 5 + 2 * 2 <= 10
