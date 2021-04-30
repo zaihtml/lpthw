1709569f1d6fec37bbdd63ec6a87f5a701f44b86
zaihtml <67802675+zaihtml@users.noreply.github.com>
2021-05-01 00:21:45 +0100

    Add files via upload

---
+++ ex17.py
@@ -0,0 +1,31 @@
+# this is a python script to copy one file to another
+# this uses a new command called 'exists'
+# 'exists' returns True if a file exists, based on its name as a string in an argument
+# and returns False if not 
+
+# using 'import' is a way to get free code other programmers have written
+
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv 
+
+print "Copying from %s to %s." % (from_file, to_file)
+
+# we could do these two on one line too, how?
+in_file = open(from_file)
+indata = in_file.read()
+
+print "The input file is %d bytes long." % len(indata)
+
+print "Does the output file exist? %r" % exists(to_file)
+print "Ready, hit RETURN to continue, CTRL-C to abort."
+raw_input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print "All right, well done."
+
+out_file.close()
+in_file(close)
